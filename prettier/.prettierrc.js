629875dfa9276d48abc35c903554549040daf070
zhang-bcxb <2698111472@qq.com>
2023-08-07 18:51:05 +0800

    常用代码模板文件

---
+++ prettier/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 200, // 单行最大宽度限制
+  tabWidth: 2, // 超过最大值换行
+  semi: false, // 结尾不用分号
+  singleQuote: true, // 使用单引号
+  trailingComma: 'none' // 函数后面不加逗号，如果不写这一个，在methods 最后一个函数也会加逗号，eslint会报错，多了一个逗号
+}
